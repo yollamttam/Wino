1110038a8245c9718ac4ae74969ea7f60709d6d5
matt <yollam.ttam@gmail.com>
2014-11-17 18:08:55 -0500

    Added Small Test, Seems to Confirm NN Working

--- numbersNN.py
+++ numbersNN.py
@@ -128,4 +128,93 @@ def neuralNetwork():
     return yyVal,fullpredictT
 
 
+def neuralNetwork_NoCV():
+    
+    ### Random constants and stuff
+    makePlots = 0
+    nClasses = 10
+    IL = 400
+    HL = 25
+    OL = 10
+    
+    ### This will hopefully be a method for predicting wine quality
+    ### based on the results of some chemical tests...
 
+    numbers_df = pd.read_csv('HandwrittenNumbers_XandY.txt')
+    numbers = numbers_df.values
+    #These come pre-sorted, which might fuck us up. Let's shuffle them
+    np.random.shuffle(numbers)
+
+    #ok, great, we can get our feature matrices, X, and 
+    #answer vectors, y
+    nFeatures = np.shape(numbers)[1]-1
+    numbersX = numbers[:,0:nFeatures]
+    numbersY = numbers[:,nFeatures]
+    
+
+    numbersX_train = numbersX
+    numbersY_train = numbersY
+
+    print "Sizes of training and cv data:"
+    print np.shape(numbersX_train)
+    
+    #let's actually build a yy matrix
+    m = np.shape(numbersX_train)[0]
+    
+            
+    # this builds a mxn matrix indicating if the mth example belongs 
+    # to the nth class
+    yy = np.zeros((m,nClasses))
+    
+    for i in range(0,m):
+        for j in range(0,nClasses):
+            if (numbersY_train[i] == j+1):
+                yy[i,j] = 1
+                
+                
+                    
+    X = numbersX_train
+    
+
+    # set up NN architecture
+    net = buildNetwork(IL,HL,OL, outclass=SigmoidLayer)
+    ds = SupervisedDataSet(nFeatures,nClasses)
+    # add numbers data to dataset
+    for i in range(0,m):
+        ds.addSample(X[i,:],yy[i,:])
+        
+    print "successfully loaded %d training examples..."%(m)
+        
+    # run backprop neural network trainer
+    trainer = BackpropTrainer(net, ds, learningrate = 0.5)
+    trainer.trainUntilConvergence(verbose = False, continueEpochs = 2, validationProportion=0.1)
+
+    # run trained neural network on cross validation set
+    fullpredict = net.activateOnDataset(ds)
+    print fullpredict 
+        
+    # creat mval x n matrix for predictions
+    fullpredictT = np.zeros(np.shape(fullpredict))
+    for i in range(0,m):
+        for j in range(0,nClasses):
+            if (fullpredict[i,j] == max(fullpredict[i,:])):
+                fullpredictT[i,j] = 1
+                
+    # print success as calculated by metric module
+    success = metric.precision_score(yy,fullpredictT)
+    print success
+
+        
+    # build confusion matrix 
+    cvvec = np.zeros((m,1))
+    predvec = np.zeros((m,1))
+    for i in range(0,m):
+        for j in range(0,nClasses):
+            if (yy[i,j] == 1):
+                cvvec[i] = j + 1
+            if (fullpredictT[i,j] == 1):
+                predvec[i] = j + 1
+        
+    confuseMat = metric.confusion_matrix(cvvec,predvec)
+    print confuseMat
+    return yy,fullpredictT
